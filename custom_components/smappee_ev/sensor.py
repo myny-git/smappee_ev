15ed2ab838c1efa412e93152a38d95b5f1c7459a
gvnuland <82335588+gvnuland@users.noreply.github.com>
2024-12-11 09:48:48 +0100

    Update sensor.py

--- custom_components/smappee_ev/sensor.py
+++ custom_components/smappee_ev/sensor.py
@@ -25,7 +25,7 @@ def async_setup_entry(
   
      """Set up the Hello State component. """
     # Get the text from the configuration. Use DEFAULT_TEXT if no name is provided.
-    text = config_entry[DOMAIN].get(CONF_TEXT, DEFAULT_TEXT)
+    text = DEFAULT_TEXT
 
     # States are in the format DOMAIN.OBJECT_ID
     hass.states.set("hello_state.Hello_State", text)
