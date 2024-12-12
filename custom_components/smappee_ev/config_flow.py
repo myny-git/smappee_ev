afd76d50df3482d119a320732f2f7bb8a25b1466
gvnuland <82335588+gvnuland@users.noreply.github.com>
2024-12-12 19:41:10 +0100

    Update config_flow.py

--- custom_components/smappee_ev/config_flow.py
+++ custom_components/smappee_ev/config_flow.py
@@ -63,6 +63,6 @@ class smappee_evFlowHandler(config_entries.OptionsFlow):
         _LOGGER.debug("evFlowHandler_start...")
         self.async_create_entry(title="Smappee EV", data=user_input)
         _LOGGER.debug("evFlowHandler_start...done")
-        _LOGGER.debug(self.config_entra.data.get("serial"))
+        _LOGGER.debug(self.config_entry.data.get("serial"))
 
         return true
