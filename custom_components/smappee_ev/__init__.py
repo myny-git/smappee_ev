e664a24a0f97872fd8914eb3d271c7660e44e70c
gvnuland <82335588+gvnuland@users.noreply.github.com>
2024-12-15 10:37:25 +0100

    Update __init__.py

--- custom_components/smappee_ev/__init__.py
+++ custom_components/smappee_ev/__init__.py
@@ -3,7 +3,7 @@ from homeassistant.core import HomeAssistant
 
 from .oauth import OAuth2Client
 from .api_client import SmappeeApiClient
-from .const import DOMAIN
+from .const import (DOMAIN, CONF_CLIENT_ID, CONF_CLIENT_SECRET, CONF_USERNAME, CONF_PASSWORD, CONF_SERIAL)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -12,7 +12,7 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     """Set up Smappee Charging Profiles from a config entry."""
     
     _LOGGER.debug("Setting up entry for Smappee EV. Serial: ")
-    _LOGGER.debug(entry.data.get("serial"))
+    _LOGGER.debug(entry.data.get(CONF_SERIAL))
     # Initialize the API client
     _LOGGER.debug("Init OAuth...")
     oauth_client = OAuth2Client(entry.data)
@@ -35,7 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     # Register the set_charging_mode service (now called actions in Home Assistant)
     async def set_charging_mode_service(call):
         """Handle the action to set the charging mode."""
-        serial = call.data.get("serial")
+        serial = call.data.get(CONF_SERIAL)
         mode = call.data.get("mode")
         limit = call.data.get("limit", 0)
 
