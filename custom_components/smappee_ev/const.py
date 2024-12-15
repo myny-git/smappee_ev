bb5cc3fab50fa1cf5e8f21fddaefb16239a8962f
gvnuland <82335588+gvnuland@users.noreply.github.com>
2024-12-15 10:33:05 +0100

    Update const.py

--- custom_components/smappee_ev/const.py
+++ custom_components/smappee_ev/const.py
@@ -1,1 +1,7 @@
 DOMAIN = "smappee_ev"
+
+CONF_CLIENT_ID: str = "client_id"
+CONF_CLIENT_SECRET: str = "client_secret"
+CONF_USERNAME: str = "username"
+CONF_PASSWORD: str = "password"
+CONF_SERIAL: str = "serial"
