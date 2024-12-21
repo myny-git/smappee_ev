ff81befde60bd1b677ac4ea0a251ae275c324794
gvnuland <82335588+gvnuland@users.noreply.github.com>
2024-12-21 10:40:35 +0100

    Update api_client.py

--- custom_components/smappee_ev/api_client.py
+++ custom_components/smappee_ev/api_client.py
@@ -5,9 +5,10 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class SmappeeApiClient:
-    def __init__(self, oauth_client):
+    def __init__(self, oauth_client, serial):
         self.oauth_client = oauth_client
         self.base_url = "https://app1pub.smappee.net/dev/v3"
+        self.serial = serial
 
     async def update():
         return True
@@ -24,6 +25,33 @@ class SmappeeApiClient:
 
     async def check_action_status():
         return True
+
+    async def fetchLatestSessionCounter(self):
+        """Set the charging mode for the given serial number and connector."""
+        # Ensure token is refreshed if needed
+        await self.oauth_client.ensure_token_valid()
+
+        url = f"{self.base_url}/chargingstations/{self.serial}/sessions?active=true&range={midnight.timestamp()}"
+        headers = {
+            "Authorization": f"Bearer {self.oauth_client.access_token}",
+            "Content-Type": "application/json",
+        }
+        _LOGGER.debug(f"Sending request to {url}")
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                response = await session.get(url, headers=headers)
+                if response.status != 200:
+                    if response.status == 401:
+                        raise Exception("Token expired")
+                    error_message = await response.text()
+                    _LOGGER.error(f"Failed to set charging mode: {error_message}")
+                    raise Exception(f"Error setting charging mode: {error_message}")
+                _LOGGER.debug(response.text())
+                return 10
+        except Exception as e:
+            _LOGGER.error(f"Exception occurred while getting latest session counter: {str(e)}")
+            raise
     
     async def set_charging_mode(self, serial, mode, limit):
         """Set the charging mode for the given serial number and connector."""
