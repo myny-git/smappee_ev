1247772bdc7cfa87f8ee102f48b0e0f73809382d
gvnuland <82335588+gvnuland@users.noreply.github.com>
2025-01-12 19:17:15 +0100

    Update api_client.py

--- custom_components/smappee_ev/api_client.py
+++ custom_components/smappee_ev/api_client.py
@@ -42,7 +42,7 @@ class SmappeeApiClient:
         midnight = datetime(now.year, now.month, now.day)
 #        _LOGGER.debug(int(midnight.timestamp()))
 
-        url = f"{self.base_url}/chargingstations/{self.serial}/sessions?active=true&range={int(midnight.timestamp())}"
+        url = f"{self.base_url}/chargingstations/{self.serial}/sessions?active=false&range=1"
         headers = {
             "Authorization": f"Bearer {self.oauth_client.access_token}",
             "Content-Type": "application/json",
