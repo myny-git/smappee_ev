6d33ab4c23cf24fba6f3a327df767259f3d18b1c
gvnuland <82335588+gvnuland@users.noreply.github.com>
2025-01-14 19:24:09 +0100

    Update api_client.py

--- custom_components/smappee_ev/api_client.py
+++ custom_components/smappee_ev/api_client.py
@@ -100,56 +100,29 @@ class SmappeeApiClient:
             self._timer = datetime.now()
             self._loop.create_task(self.delayed_update())
         return self._latestSessionCounter
-
-        # Ensure token is refreshed if needed
-#        asyncio.run(self.oauth_client.ensure_token_valid())
-#        url = f"{self.base_url}/chargingstations/{self.serial}/sessions?active=true&range={midnight.timestamp()}"
-#        headers = {
-#            "Authorization": f"Bearer {self.oauth_client.access_token}",
-#            "Content-Type": "application/json",
-#        }
-#        _LOGGER.debug(f"Sending request to {url}")
-
-#        try:
-#            async with aiohttp.ClientSession() as session:
-#                response = await session.get(url, headers=headers)
-#                if response.status != 200:
-#                    if response.status == 401:
-#                        raise Exception("Token expired")
-#                    error_message = await response.text()
-#                    _LOGGER.error(f"Failed to set charging mode: {error_message}")
-#                    raise Exception(f"Error setting charging mode: {error_message}")
-#                _LOGGER.debug(response.text())
-#                return 10
-#        except Exception as e:
-#            _LOGGER.error(f"Exception occurred while getting latest session counter: {str(e)}")
-#            raise
-    
-    async def set_charging_mode(self, serial, mode, limit):
+   
+    async def set_charging_mode(self, mode, limit):
         """Set the charging mode for the given serial number and connector."""
         # Ensure token is refreshed if needed
         await self.oauth_client.ensure_token_valid()
 
-        url = f"{self.base_url}/chargingstations/{serial}/connectors/1/mode"
+        url = f"{self.base_url}/chargingstations/{self.serial}/connectors/1/mode"
         headers = {
             "Authorization": f"Bearer {self.oauth_client.access_token}",
             "Content-Type": "application/json",
         }
 
-        limitPercentage = True if mode == "NORMAL_PERCENTAGE" else False
-        if limitPercentage:
-            mode = "NORMAL"
-
         # Create the base payload with the mode
-        payload = {"mode": mode}
-
+        if mode == "NORMAL_PERCENTAGE":
+            payload = {"mode": "NORMAL"}
+        else
+            payload = {"mode": mode}
+        
         # Add the limit only if the mode is NORMAL
+        if mode == "NORMAL_PERCENTAGE":
+            payload["limit"] = {"unit": "PERCENTAGE", "value": limit}
         if mode == "NORMAL":
-            if limitPercentage:
-                payload["limit"] = {"unit": "PERCENTAGE", "value": limit}
-            else:
-                payload["limit"] = {"unit": "AMPERE", "value": limit}
-
+            payload["limit"] = {"unit": "AMPERE", "value": limit}
         _LOGGER.debug(f"Sending request to {url} with payload {payload}")
 
         # Make the API request to set the charging mode
