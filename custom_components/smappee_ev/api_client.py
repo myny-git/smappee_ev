4b921d4c220e8e3d622fe8fd06f4c6800ede53c2
gvnuland <82335588+gvnuland@users.noreply.github.com>
2025-02-08 20:18:54 +0100

    Update api_client.py

--- custom_components/smappee_ev/api_client.py
+++ custom_components/smappee_ev/api_client.py
@@ -19,7 +19,7 @@ class SmappeeApiClient:
         self._callbacks = set()
         self._loop = asyncio.get_event_loop()
         self._latestSessionCounter = 0
-        self._state = "Unavailable"
+        self._state = "Initialize"
         self._timer = datetime.now() - timedelta(seconds = 10)
         _LOGGER.info("SmappeeApiClient init...done")
 
@@ -61,9 +61,10 @@ class SmappeeApiClient:
                     raise Exception(f"Failed to get charging sessions: {error_message}")
                 #_LOGGER.debug(f"200 Response API: {json.dumps(await response.json(), indent=5)}")
                 sessions = await response.json()
-                self._latestSessionCounter = sessions[0]["startReading"]+sessions[0]["energy"]
+                _LOGGER.debug("Set status...")
                 self._state = sessions[0]["status"]
                 _LOGGER.debug(sessions[0]["status"])
+                self._latestSessionCounter = sessions[0]["startReading"]+sessions[0]["energy"]
         except Exception as e:
             _LOGGER.error(f"Exception occurred while getting latest session counter: {str(e)}")
             raise
