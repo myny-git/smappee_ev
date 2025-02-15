e06b8320cd69890c4080087c0a13e56cf35edfbc
gvnuland <82335588+gvnuland@users.noreply.github.com>
2025-02-15 15:32:46 +0100

    Update __init__.py

--- custom_components/smappee_ev/__init__.py
+++ custom_components/smappee_ev/__init__.py
@@ -35,21 +35,33 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-            
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-            
-    # Register the set_charging_mode service (now called actions in Home Assistant)
-    @callback
-    def set_charging_mode_service(call):
-        """Handle the action to set the charging mode."""
-        _LOGGER.debug('SET CHARGING MODE SERVICE: Received data', call.data)
-        serial = call.data.get(CONF_SERIAL)
-        mode = call.data.get("mode")
-        limit = call.data.get("limit", 0)
 
-        _LOGGER.info(f"Setting charging mode for serial {serial} to {mode} with limit {limit}.")
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the an async service example component."""
+    @callback
+    def my_service(call: ServiceCall) -> None:
+        """My first service."""
+        _LOGGER.info('Received data', call.data)
+
+    # Register our service with Home Assistant.
+    hass.services.async_register(DOMAIN, 'demo', my_service)
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
+
+#async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:           
+    # Register the set_charging_mode service (now called actions in Home Assistant)
+    #@callback
+    #def set_charging_mode_service(call):
+    #    """Handle the action to set the charging mode."""
+    #    _LOGGER.debug('SET CHARGING MODE SERVICE: Received data', call.data)
+    #    serial = call.data.get(CONF_SERIAL)
+    #    mode = call.data.get("mode")
+    #    limit = call.data.get("limit", 0)
+
+    #    _LOGGER.info(f"Setting charging mode for serial {serial} to {mode} with limit {limit}.")
        
-        api_client = hass.data[DOMAIN][entry.entry_id]
+    #    api_client = hass.data[DOMAIN][entry.entry_id]
     
         #try:
         #    await api_client.set_charging_mode(serial, mode, limit)
@@ -58,7 +70,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
         #    _LOGGER.error(f"Failed to set charging mode for {serial}: {e}")
         #    raise  # Ensures that the exception is re-raised and properly logged
             
-    _LOGGER.debug('Set charging mode service in HA...')
-    hass.services.async_register(DOMAIN, "set_charging_mode", set_charging_mode_service)
-    _LOGGER.debug('Set charging mode service in HA...done')
-    return True
+    #_LOGGER.debug('Set charging mode service in HA...')
+    #hass.services.async_register(DOMAIN, "set_charging_mode", set_charging_mode_service)
+    #_LOGGER.debug('Set charging mode service in HA...done')
+    #return True
