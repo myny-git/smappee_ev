9f3f51e5fd0ebb7df1ea165a66abe0b06095c590
gvnuland <82335588+gvnuland@users.noreply.github.com>
2025-02-15 10:21:35 +0100

    Update services.py

--- custom_components/smappee_ev/services.py
+++ custom_components/smappee_ev/services.py
@@ -2,8 +2,6 @@ import logging
 from typing import cast
 from datetime import datetime
 
-
-from homeassistant.const 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import ServiceCall, callback, HomeAssistant
 from homeassistant.helpers import device_registry
