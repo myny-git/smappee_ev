a6d0eeb9a2865f0b7da6cc16d38d83c60478549b
myny-git <kris.myny@telenet.be>
2025-08-12 08:12:24 +0200

    fix current change

--- custom_components/smappee_ev/api_client.py
+++ custom_components/smappee_ev/api_client.py
@@ -200,6 +200,7 @@ class SmappeeApiClient:
             raise RuntimeError(f"set_percentage_limit failed: {text}")
         _LOGGER.debug("Set percentage limit successfully to %d%%", percentage)
         self.selected_percentage_limit = percentage
+        self.selected_current_limit = int(round((percentage / 100) * (self.max_current - self.min_
 
     async def set_available(self) -> None:
         await self.ensure_auth()
