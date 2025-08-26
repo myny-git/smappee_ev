1f1c498f9cbef2d75de6ea9deb9b6eb5420c3cb8
myny-git <kris.myny@telenet.be>
2025-08-26 15:32:10 +0200

    ruff

--- custom_components/smappee_ev/config_flow.py
+++ custom_components/smappee_ev/config_flow.py
@@ -29,15 +29,17 @@ class SmappeeEvConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         session = async_get_clientsession(self.hass)
 
-        data_schema = vol.Schema({
-            vol.Required(CONF_CLIENT_ID): str,
-            vol.Required(CONF_CLIENT_SECRET): str,
-            vol.Required(CONF_USERNAME): str,
-            vol.Required(CONF_PASSWORD): str,
-            vol.Optional(CONF_UPDATE_INTERVAL, default=UPDATE_INTERVAL_DEFAULT): vol.All(
-                int, vol.Range(min=5, max=3600)
-            ),
-        })
+        data_schema = vol.Schema(
+            {
+                vol.Required(CONF_CLIENT_ID): str,
+                vol.Required(CONF_CLIENT_SECRET): str,
+                vol.Required(CONF_USERNAME): str,
+                vol.Required(CONF_PASSWORD): str,
+                vol.Optional(CONF_UPDATE_INTERVAL, default=UPDATE_INTERVAL_DEFAULT): vol.All(
+                    int, vol.Range(min=5, max=3600)
+                ),
+            }
+        )
 
         if user_input is None:
             return self.async_show_form(
@@ -80,18 +82,24 @@ class SmappeeEvOptionsFlow(config_entries.OptionsFlow):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> ConfigFlowResult:
-        data_schema = vol.Schema({
-            vol.Required(CONF_CLIENT_ID, default=self.config_entry.data.get(CONF_CLIENT_ID)): str,
-            vol.Required(
-                CONF_CLIENT_SECRET, default=self.config_entry.data.get(CONF_CLIENT_SECRET)
-            ): str,
-            vol.Required(CONF_USERNAME, default=self.config_entry.data.get(CONF_USERNAME)): str,
-            vol.Required(CONF_PASSWORD, default=self.config_entry.data.get(CONF_PASSWORD)): str,
-            vol.Optional(
-                CONF_UPDATE_INTERVAL,
-                default=self.config_entry.data.get(CONF_UPDATE_INTERVAL, UPDATE_INTERVAL_DEFAULT),
-            ): vol.All(int, vol.Range(min=5, max=3600)),
-        })
+        data_schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_CLIENT_ID, default=self.config_entry.data.get(CONF_CLIENT_ID)
+                ): str,
+                vol.Required(
+                    CONF_CLIENT_SECRET, default=self.config_entry.data.get(CONF_CLIENT_SECRET)
+                ): str,
+                vol.Required(CONF_USERNAME, default=self.config_entry.data.get(CONF_USERNAME)): str,
+                vol.Required(CONF_PASSWORD, default=self.config_entry.data.get(CONF_PASSWORD)): str,
+                vol.Optional(
+                    CONF_UPDATE_INTERVAL,
+                    default=self.config_entry.data.get(
+                        CONF_UPDATE_INTERVAL, UPDATE_INTERVAL_DEFAULT
+                    ),
+                ): vol.All(int, vol.Range(min=5, max=3600)),
+            }
+        )
         if user_input is None:
             return self.async_show_form(step_id="init", data_schema=data_schema)
         return self.async_create_entry(title="Smappee EV", data=user_input)
