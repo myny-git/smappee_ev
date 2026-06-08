061f5417df2e07fda78c29871df1194eb4dec41b
Geert Meersman <geertmeersman@gmail.com>
2026-06-08 13:34:01 +0200

    feat: Add ConnectorCarConnectedBinarySensor to track car connection status

--- custom_components/smappee_ev/binary_sensor.py
+++ custom_components/smappee_ev/binary_sensor.py
@@ -4,12 +4,13 @@ from homeassistant.components.binary_sensor import BinarySensorDeviceClass, Bina
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.restore_state import RestoreEntity
 
 from .api_client import SmappeeApiClient
-from .base_entities import SmappeeStationEntity
+from .base_entities import SmappeeConnectorEntity, SmappeeStationEntity
 from .coordinator import SmappeeCoordinator
-from .data import SmappeeEvConfigEntry
-from .helpers import station_serial
+from .data import ConnectorState, IntegrationData, SmappeeEvConfigEntry
+from .helpers import build_connector_label, station_serial
 
 
 def _station_serial(coord: SmappeeCoordinator) -> str:
@@ -30,7 +31,10 @@ async def async_setup_entry(
         for st_uuid, bucket in (stations or {}).items():
             coord: SmappeeCoordinator = bucket["coordinator"]
             st_client: SmappeeApiClient = bucket["station_client"]
+            conns: dict[str, SmappeeApiClient] = bucket.get("connector_clients", {})
             entities.append(SmappeeMqttConnectivity(coord, st_client, sid, st_uuid))
+            for cuuid, client in (conns or {}).items():
+                entities.append(ConnectorCarConnectedBinarySensor(coord, client, sid, st_uuid, cuuid))
 
     async_add_entities(entities, True)
 
@@ -74,3 +78,64 @@ class SmappeeMqttConnectivity(SmappeeStationEntity, BinarySensorEntity):
             "station_serial": self._serial,
             "station_uuid": self._station_uuid,
         }
+
+class ConnectorCarConnectedBinarySensor(SmappeeConnectorEntity, BinarySensorEntity, RestoreEntity):
+    """Binary sensor indicating whether the car is physically connected to the charging station."""
+
+    _attr_device_class = BinarySensorDeviceClass.PLUG
+    _attr_has_entity_name = True
+
+    def __init__(
+        self,
+        coordinator: SmappeeCoordinator,
+        api_client: SmappeeApiClient,
+        sid: int,
+        station_uuid: str,
+        connector_uuid: str,
+    ) -> None:
+        num_lbl = build_connector_label(api_client, connector_uuid).split(" ", 1)[1]
+        SmappeeConnectorEntity.__init__(
+            self,
+            coordinator,
+            sid,
+            station_uuid,
+            connector_uuid,
+            unique_suffix="switch:charging",
+            name=f"Connector {num_lbl} Car Connected",
+        )
+        self.api_client = api_client
+
+    # ---------- Helpers ----------
+
+    def _conn_state(self) -> ConnectorState | None:
+        data: IntegrationData | None = self.coordinator.data
+        if not data:
+            return None
+        return (data.connectors or {}).get(self._uuid)
+
+    # ---------- HA hooks ----------
+
+    @property
+    def device_info(self):
+        return super().device_info
+
+    @property
+    def is_on(self) -> bool:
+        conn_state = self._conn_state()
+        if not conn_state:
+            return False
+
+        status = getattr(conn_state, "status_current", None)
+        if status is not None:
+            state_upper = str(status).upper()
+            return state_upper in ["CABLE_CONNECTED", "CHARGING", "SUSPENDED_EV", "SUSPENDED_EVSE"]
+
+        return False
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added to hass."""
+        await super().async_added_to_hass()
+
+        last_state = await self.async_get_last_state()
+        if last_state is not None:
+            self._attr_is_on = last_state.state == "on"
